7f6db8750bb3fb366e4e8c67c604c74714af257f
dawidkaszuba <d.kaszuba89@gmail.com>
2019-01-01 00:59:21 +0100

    first commit in 2019

---
+++ src/main/java/controller/CustomersList.java
@@ -0,0 +1,28 @@
+package controller;
+
+import dao.CustomerDao;
+import database.DbUtil;
+import model.Customer;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/CustomersList")
+public class CustomerList extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Customer> customers = CustomerDao.findAll();
+        request.setAttribute("customers", customers);
+        getServletContext().getRequestDispatcher("/WEB-INF/view/customerslist.jsp").forward(request, response);
+
+    }
+}
