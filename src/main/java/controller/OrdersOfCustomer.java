506bd2fd18027315b57a4df0e1f697a089f0f79b
dawidkaszuba <d.kaszuba89@gmail.com>
2019-01-13 00:21:57 +0100

    added view: orders by customer

--- src/main/java/controller/OrdersOfCustomer.java
+++ src/main/java/controller/OrdersOfCustomer.java
@@ -1,11 +1,15 @@
 package controller;
 
+import dao.OrderDao;
+import model.Order;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/OrdersOfCustomer")
 public class OrdersOfCustomer extends HttpServlet {
@@ -16,7 +20,10 @@ public class OrdersOfCustomer extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         if(request.getParameter("id") != null){
-
+            int id = Integer.parseInt(request.getParameter("id"));
+            List<Order> orders = OrderDao.findOrderByCustomerId(id);
+            request.setAttribute("orders",orders);
+            getServletContext().getRequestDispatcher("/WEB-INF/view/ordersOfCustomer.jsp").forward(request,response);
         }
 
     }
--- src/main/java/dao/OrderDao.java
+++ src/main/java/dao/OrderDao.java
@@ -258,4 +258,37 @@ public class OrderDao {
 
         return list;
     }
+
+    public static List<Order> findOrderByCustomerId(int id) {
+        List<Order> orders = new ArrayList<>();
+        String sql = " SELECT * FROM orders WHERE idOfCustomer=?";
+        try(Connection connection = DbUtil.getConn()){
+            try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+                preparedStatement.setInt(1,id);
+                ResultSet rs = preparedStatement.executeQuery();
+                while(rs.next()){
+                    Order order = new Order();
+                    order.setId(rs.getInt("id"));
+                    order.setDateOfAcceptanceForRepair(rs.getObject("dateOfAcceptanceForRepair", LocalDate.class));
+                    order.setPlannedRepairDate(rs.getObject("plannedRepairDate", LocalDate.class));
+                    order.setStartedDateOfRepair(rs.getObject("startedDateOfRepair", LocalDate.class));
+                    order.setIdOfEmployee(rs.getInt("idOfEmployee"));
+                    order.setDescriptionOfProblem(rs.getString("descriptionOfProblem"));
+                    order.setStatus(rs.getString("status"));
+                    order.setIdOfVehicle(rs.getInt("idOfVehicle"));
+                    order.setCostOfWork(rs.getDouble("costOfWork"));
+                    order.setCostOfAutoParts(rs.getDouble("costOfAutoParts"));
+                    order.setCostOfWorkHour(rs.getDouble("costOfWorkHour"));
+                    order.setQuantityOfWorkHour(rs.getDouble("quantityOfWorkHour"));
+                    order.setIdOfCustomer(rs.getInt("idOfCustomer"));
+                    orders.add(order);
+
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return orders;
+    }
 }
