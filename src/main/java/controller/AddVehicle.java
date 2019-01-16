722159691bedb497dc3d764f0dd2fcf09de2a031
dawidkaszuba <d.kaszuba89@gmail.com>
2019-01-16 21:17:55 +0100

    improved AddVehicle

--- src/main/java/controller/AddVehicle.java
+++ src/main/java/controller/AddVehicle.java
@@ -18,51 +18,42 @@ import java.time.LocalDate;
 public class AddVehicle extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String brand;
+        String model;
+        String nextTechnicalReview;
+        String registrationNumber;
+        int yearOfProduction;
+        int idOfOwner;
+
         try{
-            Vehicle vehicle = new Vehicle();
-            if(request.getParameter("brand") != null) {
-                vehicle.setBrand(request.getParameter("brand"));
-            }else{
-                response.getWriter().append("Brak marki");
-            }
-            if(request.getParameter("model") != null) {
-                vehicle.setModel(request.getParameter("model"));
-            }else{
-                response.getWriter().append("Brak modelu");
-            }
-            if(request.getParameter("nextTechnicalReview") != null) {
-                vehicle.setNextTechnicalReview(LocalDate.parse(request.getParameter("nextTechnicalReview")));
-            }else{
-                response.getWriter().append("Brak nextTechnicalRewwiev");
-            }
-            if(request.getParameter("registrationNumber") != null) {
-                vehicle.setRegistrationNumber(request.getParameter("registrationNumber"));
-            }else{
-                response.getWriter().append("Brak registrationNumber");
-            }
-            if(request.getParameter("yearOfProduction") != null) {
-                vehicle.setYearOfProduction(Integer.parseInt(request.getParameter("yearOfProduction")));
-            }else{
-                response.getWriter().append("Brak yearOfProduction");
-            }
-            if(request.getParameter("idOfOwner") != null) {
-                vehicle.setIdOfOwner(Integer.parseInt(request.getParameter("idOfOwner")));
-            }else{
-                response.getWriter().append("Brak idOfOwner");
-            }
+            brand = request.getParameter("brand");
+            model = request.getParameter("model");
+            nextTechnicalReview = request.getParameter("nextTechnicalReview");
+            registrationNumber = request.getParameter("registrationNumber");
+            yearOfProduction = Integer.parseInt(request.getParameter("yearOfProduction"));
+            idOfOwner = Integer.parseInt(request.getParameter("idOfOwner"));
 
-            try {
+            if(brand != "" && model != "" && nextTechnicalReview != "" && registrationNumber != "") {
+
+                Vehicle vehicle = new Vehicle();
+                vehicle.setBrand(brand);
+                vehicle.setModel(model);
+                vehicle.setNextTechnicalReview(LocalDate.parse(nextTechnicalReview));
+                vehicle.setRegistrationNumber(registrationNumber);
+                vehicle.setYearOfProduction(yearOfProduction);
+                vehicle.setIdOfOwner(idOfOwner);
+
                 VehicleDao.save(DbUtil.getConn(), vehicle);
-            } catch (SQLException e) {
-                e.printStackTrace();
+                response.sendRedirect("/VehiclesList");
+            }else{
+                response.sendRedirect("/Error1");
             }
 
-
-        }catch(NumberFormatException e) {
-            response.getWriter().append("Brak dancyh");
+        }catch(NumberFormatException e){
+            response.sendRedirect("/Error1");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        response.sendRedirect("/VehiclesList");
 
     }
 
