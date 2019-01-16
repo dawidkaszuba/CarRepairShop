401fa64e4f30a68534f856d0e02aa8dc07912ec8
dawidkaszuba <d.kaszuba89@gmail.com>
2019-01-16 22:24:43 +0100

    improved EditOrder

--- src/main/java/controller/EditOrder.java
+++ src/main/java/controller/EditOrder.java
@@ -12,113 +12,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.List;
 
 @WebServlet("/EditOrder")
 public class EditOrder extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
 
-        double profit =0;
+        int id;
+        String dateOfAcceptanceForRepair;
+        String plannedRepairDate;
+        String startedDateOfRepair;
+        int idOfEmployee;
+        String descriptionOfProblem;
+        String status;
+        int idOfVehicle;
+        double costOfWork;
+        double costOfAutoParts;
+        double costOfWorkHour;
+        int quantityOfWorkHour;
+        int idOfCustomer;
+        double profit = 0;
+
         try {
-            Order order = new Order();
-            if(request.getParameter("id")!=null){
-                int id  = Integer.parseInt(request.getParameter("id"));
+            id = Integer.parseInt(request.getParameter("id"));
+            dateOfAcceptanceForRepair = request.getParameter("dateOfAcceptanceForRepair");
+            plannedRepairDate = request.getParameter("plannedRepairDate");
+            startedDateOfRepair = request.getParameter("startedDateOfRepair");
+            idOfEmployee = Integer.parseInt(request.getParameter("idOfEmployee"));
+            descriptionOfProblem = request.getParameter("descriptionOfProblem");
+            status = request.getParameter("status");
+            idOfVehicle = Integer.parseInt(request.getParameter("idOfVehicle"));
+            costOfAutoParts = Double.parseDouble(request.getParameter("costOfAutoParts"));
+            quantityOfWorkHour = Integer.parseInt(request.getParameter("quantityOfWorkHour"));
+            profit = Double.parseDouble(request.getParameter("profit"));
+
+            if (dateOfAcceptanceForRepair != "" && plannedRepairDate != "" && startedDateOfRepair != "" &&
+                    descriptionOfProblem != "" && status != "") {
+                Order order = new Order();
                 order.setId(id);
-            }
-            if(request.getParameter("profit")!=null){
-                profit = Double.parseDouble(request.getParameter("profit"));
-            }
-            if (request.getParameter("dateOfAcceptanceForRepair") != null) {
-                order.setDateOfAcceptanceForRepair(LocalDate.parse(request.getParameter("dateOfAcceptanceForRepair")));
-            }else{
-                response.getWriter().append("Brak dateOfAcceptanceForRepair");
-            }
-            if (request.getParameter("plannedRepairDate") != null) {
-                order.setPlannedRepairDate(LocalDate.parse(request.getParameter("plannedRepairDate")));
-            }else{
-                response.getWriter().append("Brak plannedRepairDate");
-            }
-            if (request.getParameter("startedDateOfRepair") != null) {
-                order.setStartedDateOfRepair(LocalDate.parse(request.getParameter("startedDateOfRepair")));
-            }else{
-                response.getWriter().append("Brak startedDateOfRepair");
-            }
-            if (request.getParameter("idOfEmployee") != null) {
-                order.setIdOfEmployee(Integer.parseInt(request.getParameter("idOfEmployee")));
-            }else{
-                response.getWriter().append("Brak idOfEmployee");
-            }
-            if (request.getParameter("descriptionOfProblem") != null) {
-                order.setDescriptionOfProblem(request.getParameter("descriptionOfProblem"));
-            }else{
-                response.getWriter().append("Brak descriptionOfProblem");
-            }
-            if (request.getParameter("status") != null) {
-                order.setStatus(request.getParameter("status"));
-            }else{
-                response.getWriter().append("Brak status");
-            }
-            if (request.getParameter("idOfVehicle") != null) {
-                order.setIdOfVehicle(Integer.parseInt(request.getParameter("idOfVehicle")));
-            }else{
-                response.getWriter().append("Brak idOfVehicle");
-            }
-
-            if (request.getParameter("costOfAutoParts") != null) {
-                order.setCostOfAutoParts(Double.parseDouble(request.getParameter("costOfAutoParts")));
-            }else{
-                response.getWriter().append("Brak costOfAutoParts");
-            }
-
-            if (request.getParameter("quantityOfWorkHour") != null) {
-                order.setQuantityOfWorkHour(Integer.parseInt(request.getParameter("quantityOfWorkHour")));
-            }else{
-                response.getWriter().append("Brak quantityOfWorkHour");
-            }
-
-            if (request.getParameter("idOfVehicle") != null) {
+                order.setDateOfAcceptanceForRepair(LocalDate.parse(dateOfAcceptanceForRepair));
+                order.setPlannedRepairDate(LocalDate.parse(plannedRepairDate));
+                order.setStartedDateOfRepair(LocalDate.parse(startedDateOfRepair));
+                order.setIdOfEmployee(idOfEmployee);
+                order.setDescriptionOfProblem(descriptionOfProblem);
+                order.setStatus(status);
+                order.setIdOfVehicle(idOfVehicle);
+                order.setCostOfAutoParts(costOfAutoParts);
+                order.setQuantityOfWorkHour(quantityOfWorkHour);
                 order.setIdOfCustomer(CustomerDao.findCustomerByIdOfVehicle(Integer.parseInt(request.getParameter("idOfVehicle"))));
-            }else{
-                response.getWriter().append("Brak idOfVehicle");
+                order.setCostOfWorkHour(EmployeeDao.findById(Integer.parseInt(request.getParameter("idOfEmployee")))
+                        .getCostOfWorkHour());
+                order.setCostOfWork((((Integer.parseInt(request.getParameter("quantityOfWorkHour")))
+                        * (EmployeeDao.findById(Integer.parseInt(request.getParameter("idOfEmployee"))).getCostOfWorkHour()))
+                        + (Double.parseDouble(request.getParameter("costOfAutoParts")))) / profit);
+
+                OrderDao.save(order);
+                response.sendRedirect("/OrderList2");
+
             }
-            setCostOfWorkHour(request, order);
-            setCostOfOrder(request, order,profit);
 
-            OrderDao.save(order);
-
-        }catch(NumberFormatException e){
-            response.getWriter().append("Brak danych");
-        }catch(NullPointerException e){
-            response.getWriter().append("Brak dancyh");
-        }catch(DateTimeException e) {
-            response.getWriter().append("Brak daty");
+        } catch (NumberFormatException e) {
+            response.sendRedirect("/Error1");
         }
-
-        response.sendRedirect("/OrderList2");
-
-    }
-
-    private void setCostOfOrder(HttpServletRequest request, Order order,double profit) {
-        double costOfWork = (((Integer.parseInt(request.getParameter("quantityOfWorkHour")))
-                * (EmployeeDao.findById(Integer.parseInt(request.getParameter("idOfEmployee"))).getCostOfWorkHour()))
-                +(Double.parseDouble(request.getParameter("costOfAutoParts"))))/profit;
-        order.setCostOfWork(costOfWork);
-    }
-
-    private void setCostOfWorkHour(HttpServletRequest request, Order order) {
-        double costOfWorkHour = EmployeeDao.findById(Integer.parseInt(request.getParameter("idOfEmployee"))).getCostOfWorkHour();
-        System.out.println(costOfWorkHour);
-        order.setCostOfWorkHour(costOfWorkHour);
     }
 
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getParameter("id") != null){
+        if (request.getParameter("id") != null) {
 
             Order order = OrderDao.findById(Integer.parseInt(request.getParameter("id")));
             List<Employee> employees = EmployeeDao.findAll();
@@ -129,10 +94,10 @@ public class EditOrder extends HttpServlet {
             request.setAttribute("states", states);
             request.setAttribute("vehicles", vehicles);
 
-            getServletContext().getRequestDispatcher("/WEB-INF/view/editorder.jsp").forward(request,response);
+            getServletContext().getRequestDispatcher("/WEB-INF/view/editorder.jsp").forward(request, response);
 
 
-        }else{
+        } else {
             response.getWriter().append("Brak danych");
         }
 
